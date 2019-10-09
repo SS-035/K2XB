a3486f0de65d0aac395c49dfdd6e7d19e2f875d5
Shadow035 <saikatsantra4545@gmail.com>
2019-10-09 14:58:29 +0530

    blah blah blah

--- MakeConfiguration_vXB/MakeConfigWindow.Designer.cs
+++ MakeConfiguration_vXB/MakeConfigWindow.Designer.cs
@@ -28,21 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 28;
+            this.textBox1.Text = "Put your configuration name here";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(340, 393);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 29;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(431, 393);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 30;
+            this.button14.Text = "button14";
+            this.button14.UseVisualStyleBackColor = true;
             // 
             // MakeConfigurationWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 311);
+            this.ClientSize = new System.Drawing.Size(531, 428);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "MakeConfigurationWindow";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
     }
 }
 
--- MakeConfiguration_vXB/MakeConfigWindow.cs
+++ MakeConfiguration_vXB/MakeConfigWindow.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.Windows.Forms;
 using RawInput_dll;
 
@@ -10,6 +9,18 @@ namespace MakeConfiguration_vXB
     {
         private readonly RawInput _rawinput;
         const bool CaptureOnlyInForeground = true;
+        private const int numberofKeys = 1 + 174;
+        private static int isGameMode = 0;
+
+        private static readonly string[] keyArray = { "GAMEMODE", "A", "ADD", "ALT", "APPS", "ATTN", "B", "BACK", "BROWSERBACK", "BROWSERFAVORITES", "BROWSERFORWARD", "BROWSERHOME", "BROWSERREFRESH", "BROWSERSEARCH", "BROWSERSTOP", "C", "CANCEL", "CAPITAL", "CLEAR",
+            "CONTROL", "CONTROLKEY", "CRSEL", "D", "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DECIMAL", "DELETE", "DIVIDE", "DOWN", "E", "END", "ENTER", "ERASEEOF", "ESCAPE", "EXECUTE", "EXSEL", "F", "F1", "F10", "F11", "F12", "F13", "F14", "F15",
+            "F16", "F17", "F18", "F19", "F2", "F20", "F21", "F22", "F23", "F24", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "FINALMODE", "G", "H", "HANGUELMODE", "HANJAMODE", "HELP", "HOME", "I", "IMEACEEPT", "IMECONVERT", "IMEMODECHANGE", "IMENONCONVERT", "INSERT",
+            "J", "JUNJAMODE", "K", "KEYCODE", "L", "LAUNCHAPPLICATION1", "LAUNCHAPPLICATION2", "LAUNCHMAIL", "LBUTTON", "LCONTROL", "LEFT", "LINEFEED", "LMENU", "LSHIFT", "LWIN", "M", "MBUTTON", "MEDIANEXTTRACK", "MEDIAPLAYPAUSE", "MEDIAPREVIOUSTRACK", "MEDIASTOP",
+            "MENU", "MULTIPLY", "N", "NEXT", "NONAME", "NONE", "NUMLOCK", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "O", "OEM8", "OEMBACKSLASH", "OEMCLEAR", "OEMCLOSEBRACKETS", "OEMCOMMA", "OEMMINUS",
+            "OEMOPENBRACKETS", "OEMPERIOD", "OEMPIPE", "OEMPLUS", "OEMQUESTION", "OEMQUOTES", "OEMSEMICOLON", "OEMTILDE", "P", "PA1", "PAUSE", "PLAY", "PRINT","PRINTSCREEN", "PRIOR", "PROCESSKEY", "Q", "R", "RBUTTON", "RCONTROL", "RIGHT", "RMENU", "RSHIFT", "RWIN",
+            "S", "SCROLL", "SELECT", "SELECTMEDIA", "SEPARATOR", "SHIFT", "SHIFTKEY", "SPACE", "SUBTRACT", "T", "TAB", "U", "UP", "V", "VOLUMEDOWN", "VOLUMEMUTE", "VOLUMEUP", "W", "X", "XBUTTON1", "XBUTTON2", "Y", "Z", "ZOOM" };
+
+        private static string[,] controllerKeys = new string[numberofKeys, 2];
 
         public MakeConfigurationWindow()
         {
@@ -24,7 +35,22 @@ namespace MakeConfiguration_vXB
         }
         private void OnKeyPressed(object sender, RawInputEventArg e)
         {
+            int keyPosition = SearchKeyPosition(e.KeyPressEvent.VKeyName);
+            if (controllerKeys[keyPosition,isGameMode] == null)
+            {
 
+            }
+        }
+
+        private int SearchKeyPosition(string keyName)
+        {
+            int ite;
+            for (ite = 0; ite < numberofKeys; ite++)
+            {
+                if (keyName == keyArray[ite])
+                    return ite;
+            }
+            return -1;
         }
 
         private void Keyboard_FormClosing(object sender, FormClosingEventArgs e)
@@ -39,6 +65,11 @@ namespace MakeConfiguration_vXB
             Debug.WriteLine("Unhandled Exception: " + ex.Message);
             Debug.WriteLine("Unhandled Exception: " + ex);
             MessageBox.Show(ex.Message);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
